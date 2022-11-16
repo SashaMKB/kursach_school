9debe51d9c0f6c2b6216642fec82666fbf235005
Alexander <73365442+SashaMKB@users.noreply.github.com>
2022-11-16 21:09:11 +0300

    add first "home" controller

---
+++ src/main/java/com/reboticaSchool/School/Manager/SchoolManagerApplication.java
@@ -0,0 +1,13 @@
+package com.reboticaSchool.School.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagerApplication.class, args);
+	}
+
+}
