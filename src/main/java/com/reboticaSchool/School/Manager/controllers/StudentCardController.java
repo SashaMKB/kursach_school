8ce607d93d04af26e20cb7ae1348c230337b3085
Alexander <73365442+SashaMKB@users.noreply.github.com>
2022-12-16 22:20:27 +0300

    just for save

--- src/main/java/com/reboticaSchool/School/Manager/controllers/StudentCardController.java
+++ src/main/java/com/reboticaSchool/School/Manager/controllers/StudentCardController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class StudentCardController {
@@ -23,6 +27,52 @@ public class StudentCardController {
     public String studentCardAdd(@RequestParam String studentFirstName,@RequestParam String studentSecondName,@RequestParam String studentSurName,@RequestParam String studentNum,@RequestParam String studentCourse,@RequestParam String classDay,@RequestParam String classTime, Model model){
         Student student = new Student(studentFirstName,studentSecondName,studentSurName,studentNum,studentCourse,classTime,classDay);
         studentRepository.save(student);
+        return "redirect:/";
+    }
+    @GetMapping("/student-card/{id}")
+    public String studentDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!studentRepository.existsById(id)) {
+            return "redirect:/blog";
+    }
+
+        Optional<Student> student = studentRepository.findById(id);
+        ArrayList<Student> res = new ArrayList<>();
+        student.ifPresent(res::add);
+        model.addAttribute("student",res);
+        return "/student-details";
+}
+
+    @GetMapping("/student-card/{id}/edit")
+    public String studentEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!studentRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Student> student = studentRepository.findById(id);
+        ArrayList<Student> res = new ArrayList<>();
+        student.ifPresent(res::add);
+        model.addAttribute("student",res);
+        return "student-edit";
+    }
+
+    @PostMapping("/student-card/{id}/edit")
+    public String studentCardUpdate(@PathVariable(value = "id") long id,@RequestParam String studentFirstName,@RequestParam String studentSecondName,@RequestParam String studentSurName,@RequestParam String studentNum,@RequestParam String studentCourse,@RequestParam String classDay,@RequestParam String classTime, Model model){
+        Student student = studentRepository.findById(id).orElseThrow();
+        student.setStudentFirstName(studentFirstName);
+        student.setStudentSecondName(studentSecondName);
+        student.setStudentSurName(studentSurName);
+        student.setStudentNum(studentNum);
+        student.setStudentCourse(studentCourse);
+        student.setClassDay(classDay);
+        student.setClassTime(classTime);
+        studentRepository.save(student);
+        return "redirect:/";
+    }
+
+    @PostMapping("/student-card/{id}/remove")
+    public String studentCardDelete(@PathVariable(value = "id") long id,Model model){
+        Student student = studentRepository.findById(id).orElseThrow();
+        studentRepository.delete(student);
         return "redirect:/";
     }
 }
